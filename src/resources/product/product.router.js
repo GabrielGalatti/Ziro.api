6638e9cd3d82a7319da1dc2f3d121f1c9167e458
Gabriel Galatti <gabrielgalatti@hotmail.com>
2020-01-10 02:38:12 -0300

    Mudança de verb route product

--- src/resources/product/product.router.js
+++ src/resources/product/product.router.js
@@ -7,6 +7,6 @@ router.route("/").post(controller.createOne);
 
 router.route("/marca").post(searchByBrand);
 
-router.route("/:id").post(controller.findById);
+router.route("/getName").post(controller.findById);
 
 export default router;
